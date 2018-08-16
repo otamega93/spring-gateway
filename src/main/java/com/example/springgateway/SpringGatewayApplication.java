5a60fd01333b3884c1b5f283e42e2da31d7638a5
MANUEL ANGEL PAEZ <mapaez@externos.bankia.com>
2018-08-16 10:53:09 +0200

    Spring gateway microservice

--- src/main/java/com/example/springgateway/SpringGatewayApplication.java
+++ src/main/java/com/example/springgateway/SpringGatewayApplication.java
@@ -2,8 +2,10 @@ package com.example.springgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringGatewayApplication {
 
 	public static void main(String[] args) {
